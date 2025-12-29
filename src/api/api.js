673401621b2fb412251968eb66b69d9b16e54796
Devanshi Vaidya <dvaidya10@gmail.com>
2025-12-30 02:53:47 +0530

    latest changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+console.log("API BASE URL =>", import.meta.env.VITE_API_BASE_URL);
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
 
 API.interceptors.request.use(
